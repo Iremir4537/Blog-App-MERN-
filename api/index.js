dc4039133632ce7dbe4df10f5da334fff432452c
Emir Tüfekci <e.tufekci@simplydeliver.com>
2024-08-05 23:20:02 +0300

    backend setup

---
+++ api/index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+const PORT = 4000
+
+app.use(cors())
+app.use(express.json())
+
+app.post('/register',(req,res) =>{
+    const {username,password} = req.body
+    res.json({requestData:{username,password}})
+})
+
+app.listen(PORT,() => {
+    console.log(`Server is running on Port ${PORT}`)
+})
